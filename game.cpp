4c611edbf2e2d4856cdbe500dce7d289a39da1ae
Andy Simphaly <andytsimphaly@gmail.com>
2021-07-28 21:54:40 -0500

    Added some comments to the print_board function

--- game.cpp
+++ game.cpp
@@ -7,7 +7,7 @@
 game::game() {
 
     for (int i = 0; i <= 8; i++) {
-        _board[i] = '.';
+        _board[i] = ' ';
     }
 
 };
@@ -17,16 +17,20 @@ game::~game() {
 
 };
 
-
+//Funtion to print the board
 void game::print_board() {
 
-    
+    //Loop through each element of the board
     for (int i = 0; i <= 8; i++) {
+        //Print bars and the element
         std::cout << "| ";
         std::cout << _board[i];
+
+        //Check if it is the element position is a multiple of 3
         if (((i+1)%3) == 0) {
             std::cout << " | " << std::endl;
             
+            //Print a bar dividing each horizontal line
             for (int j = 0; j < 18; j++) {
                 std::cout << "-";
             }
