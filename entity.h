2a89a26f36647438889ac3b5dd09cac129a45221
Andy Simphaly <andytsimphaly@gmail.com>
2021-08-05 00:38:56 -0500

    Decided to use a entity class for each piece

---
+++ entity.h
@@ -0,0 +1,23 @@
+//Header file for the entity class
+
+//Libraries
+#include <iostream>
+
+#pragma once
+
+//Player class
+class entity {
+
+    public:
+        //Constructor and Destructor
+        entity();
+        ~entity();
+
+        //Variables
+        int _val;
+        bool _used;
+        char _color;
+
+    private:
+
+};
