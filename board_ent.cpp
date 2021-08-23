361fd971f90b3c1f045a8f37efacf17aff4d6217
Andy Simphaly <andytsimphaly@gmail.com>
2021-08-23 15:37:53 -0500

    Created a new class for the board

---
+++ board_ent.cpp
@@ -0,0 +1,4 @@
+//Function file for the board entity class
+
+//Libraries
+#include "board_ent.h"
---
+++ board_ent.h
@@ -0,0 +1,23 @@
+//Header file for the board entity class
+
+//Libraries
+#include <iostream>
+
+#pragma once
+
+//Player class
+class board_ent {
+
+    public:
+        //Constructor and Destructor
+        board_ent();
+        ~board_ent();
+
+        //Variables
+        int _val;
+        bool _used;
+        char _color;
+
+    private:
+
+};
